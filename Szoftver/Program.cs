d155f4153e27c862b7a23bbd271935429fa33605
pleszko <pleszkol@kkszki.hu>
2025-09-24 11:45:58 +0200

    Feladat2 is ready

--- Szoftver/Program.cs
+++ Szoftver/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Tracing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,16 +11,31 @@ namespace Szoftver
     {
         public class Szemely
         {
-            public string nev = "Nagy Ilona";
-            public int kor = 21;
+            private string _nev;
+            private int _kor;
 
+            public Szemely(string nev, int kor)
+            {
+                _nev = nev;
+                _kor = kor;
+            }
+            public string kiir()
+            {
+                return _nev + " " + _kor;
+            }
         }
+        public class BankSzamla
+        {
+            private string szamlaszam;
+        }
+   
         static void Main(string[] args)
         {
-            Szemely szemely = new Szemely();
-            Console.WriteLine("Név: " + szemely.nev);
-            Console.WriteLine("Kor: " + szemely.kor);
-            Console.ReadKey();
+            Szemely szemely = new Szemely("Kiss Ilona", 44);
+
+            Console.WriteLine(szemely.kiir());
+
+
         }
     }
 }
